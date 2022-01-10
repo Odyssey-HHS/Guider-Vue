c08ca9248f9987aaeba756f7dc11cb4af0b2f3e1
Wouter de Bruijn <wouter@hedium.nl>
2022-01-10 13:56:27 +0100

    :poop: Added hard sync

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,29 @@ import { reactive } from "vue";
 const store = reactive({
   websocket: new WebSocket("ws://localhost:8443/ws"),
   authenticated: false,
+  motionAlert: false,
+  door: false,
+  lampColor: 0,
 });
+
+// Add Event listener
+store.websocket.addEventListener("message", (message) => {
+  const object = JSON.parse(message.data);
+  console.log(object);
+  if (object && typeof object.motionAlert === "boolean")
+    store.motionAlert = object.motionAlert;
+
+  if (object && typeof object.door === "boolean") store.door = object.door;
+
+  if (object && typeof object.lampColor === "number")
+    store.lampColor = object.lampColor;
+});
+
+setInterval(() => {
+  console.log("Hard sync fetch");
+
+  store.websocket.send("{}");
+}, 1000);
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function useStore() {

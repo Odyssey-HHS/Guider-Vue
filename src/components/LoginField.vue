ae4b88ca45da85e18f7bdf7f0eaec0329f65cd5d
Casper Bastiaan-Net <casper.bastiaan.net@gmail.com>
2022-01-14 16:33:29 +0100

    :lock: added login

--- src/components/LoginField.vue
+++ src/components/LoginField.vue
@@ -5,6 +5,8 @@
         <h1 class="text-gray-800 text-xl">Login</h1>
       </div>
       <div class="py-2 bg-gray-200">
+        <input type="text" v-model="username" placeholder="naam" />
+        <input type="password" v-model="password" placeholder="wachtwoord" />
         <button
           class="border-black bg-green-400 py-2 px-4 rounded shadow-sm"
           @click="login"
@@ -26,14 +28,20 @@ export default defineComponent({
   data() {
     return {
       lampValue: 0,
+      username: "",
+      password: "",
     };
   },
   methods: {
     login() {
       const store = useStore();
-      store.websocket.send(JSON.stringify({ token: "hardcoded-valid" }));
-      store.authenticated = true;
-      this.$router.push("/");
+      store.websocket.send(
+        JSON.stringify({
+          token: "hardcoded-valid",
+          username: this.username,
+          password: this.password,
+        })
+      );
     },
   },
 });
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
+import { getRouter } from "@/router";
 import { reactive } from "vue";
 
 const store = reactive({
   websocket: new WebSocket("ws://localhost:8443/ws"),
   authenticated: false,
+  role: "",
   motionAlert: false,
   door: false,
   lampColor: 0,
@@ -12,6 +14,11 @@ const store = reactive({
 store.websocket.addEventListener("message", (message) => {
   const object = JSON.parse(message.data);
   console.log(object);
+  if (object && typeof object.role === "string") {
+    store.authenticated = true;
+    store.role = object.role;
+    getRouter().push("/");
+  }
   if (object && typeof object.motionAlert === "boolean")
     store.motionAlert = object.motionAlert;
 

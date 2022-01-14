e2d1faf92785d1fcdca15b276b1b925a52a8e034
Casper Bastiaan-Net <casper.bastiaan.net@gmail.com>
2022-01-14 17:11:16 +0100

    :lipstick: improved look/functionality of login

--- src/components/LoginField.vue
+++ src/components/LoginField.vue
@@ -4,15 +4,29 @@
       <div class="bg-red-500 p-2">
         <h1 class="text-gray-800 text-xl">Login</h1>
       </div>
-      <div class="py-2 bg-gray-200">
-        <input type="text" v-model="username" placeholder="naam" />
-        <input type="password" v-model="password" placeholder="wachtwoord" />
-        <button
-          class="border-black bg-green-400 py-2 px-4 rounded shadow-sm"
-          @click="login"
-        >
-          Let me in! :)
-        </button>
+      <div class="flex py-2 bg-gray-200 justify-center">
+        <div class="columns-1 w-min">
+          <input
+            type="text"
+            v-model="username"
+            placeholder="naam"
+            class="mx-2 mt-2"
+            @keydown.enter="login"
+          />
+          <input
+            type="password"
+            v-model="password"
+            placeholder="wachtwoord"
+            class="mx-2 my-2"
+            @keydown.enter="login"
+          />
+          <button
+            class="border-black bg-green-400 py-2 px-4 rounded shadow-sm"
+            @click="login"
+          >
+            Let me in! :)
+          </button>
+        </div>
       </div>
     </div>
   </div>
